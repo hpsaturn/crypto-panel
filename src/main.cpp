cbfcfcca5a0d6c6983fef7f2302d76d104faad45
Antonio Vanegas <hpsaturn@gmail.com>
2021-09-19 16:21:35 +0200

    fix log issue with vref

--- src/main.cpp
+++ src/main.cpp
@@ -191,7 +191,7 @@ void setup() {
     esp_adc_cal_characteristics_t adc_chars;
     esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
     if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-        Serial.printf("eFuse Vref:%u mV", adc_chars.vref);
+        Serial.printf("eFuse Vref:%u mV\n", adc_chars.vref);
         vref = adc_chars.vref;
     }
 
