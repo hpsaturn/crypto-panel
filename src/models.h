de7db1ff85e7e7f0b7fdfb736e5512f76448657c
Hpsaturn <hpsaturn@gmail.com>
2022-07-13 02:56:53 +0200

    v0.0.8 rev150 pre1 Crypto Panel with setup via CLI

---
+++ src/models.h
@@ -0,0 +1,37 @@
+struct Price {
+  double inr;
+  String btc;
+  String eth;
+};
+
+struct Crypto
+{
+  String apiName;
+  String symbol;
+  Price price;
+  double dayChange;
+  double weekChange;
+};
+
+struct News
+{
+  String author;
+  String title;
+  String summary;
+  String published;
+  String link;
+  uint32_t qrsize;
+  const char* qr;
+};
+
+// ----------------------------
+// Coin id list - adjust it to meet your interests
+//
+// Put your cryptocurrencies in the array below.
+// Get id of your coin here: https://api.coingecko.com/api/v3/coins/list?include_platform=false
+// ----------------------------
+
+Crypto cryptos[3];
+int cryptosCount;
+News news;
+
