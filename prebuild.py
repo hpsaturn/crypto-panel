9c75b5d043b8df00571b8e22d635152ae497358b
Hpsaturn <hpsaturn@gmail.com>
2025-01-23 20:57:16 +0100

    improved OTA and Webinstaller binaries on CanAirIO server

--- prebuild.py
+++ prebuild.py
@@ -58,21 +58,56 @@ env.Append(BUILD_FLAGS=[
     u'-I \"' + srcdir + '\"'
     ])
 
-data = {
+chipFamily = "ESP32"
+
+if flavor == "ESP32C3" or flavor == "ESP32C3OIPLUS" or flavor == "ESP32C3LOLIN" or flavor == "ESP32C3SEEDX" or flavor == "AG_OPENAIR":
+    chipFamily = "ESP32-C3"
+
+if flavor == "ESP32S3" or flavor == "TTGO_T7S3":
+    chipFamily = "ESP32-S3"
+
+manifest_fota = {
     "type":flavor, 
     "version":revision, 
     "host":"influxdb.canair.io",
     "port":8080,
-    "bin":"/releases/" + target + "/canairio_" + flavor + "_rev" + revision + ".bin"
+    "bin":"/releases/" + target + "/eInkCrypto_" + flavor + "_rev" + revision + ".bin"
 }
 
-output_path =  "releases/manifest/" + target
+bin_path = "https://influxdb.canair.io/releases/webi/"+target+"/eInkCrypto_" + flavor + "_rev" + revision + "_merged.bin"
 
-os.makedirs(output_path, 0o755, True)
+manifest_webi = {
+  "name": "CryptoPanel "+flavor,
+  "version": revision,
+  "new_install_prompt_erase": True,
+  "funding_url": "https://liberapay.com/hpsaturn",
+  "builds": [
+    {
+      "chipFamily": chipFamily,
+      "parts": [
+        { "path": bin_path, "offset": 0 }
+      ]
+    }
+  ]
+}
 
-output_manifiest = output_path + "/firmware_" + flavor + ".json"
+# build_flags = env.get("BUILD_FLAGS")
+# print("NEW BUILD_FLAGS")
+# print(build_flags)
 
-with open(output_manifiest, 'w') as outfile:
-    json.dump(data, outfile)
+manifest_dir =  "releases/manifest/" + target
+manifest_webi_dir = "releases/manifest/webi/" + target
 
+os.makedirs(manifest_dir, 0o755, True)
+os.makedirs(manifest_webi_dir, 0o755, True)
+
+manifest_fota_file = manifest_dir + "/firmware_" + flavor + ".json"
+
+with open(manifest_fota_file, 'w') as outfile:
+    json.dump(manifest_fota, outfile)
+
+manifest_webi_file = manifest_webi_dir + "/firmware_" + flavor + ".json"
+
+with open(manifest_webi_file, 'w') as outfile:
+    json.dump(manifest_webi, outfile)
 
